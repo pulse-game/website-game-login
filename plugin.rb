8b6753d9f88e981a92cc86aeba56d166844af29c
Ranama <Jeppeandreasson@gmail.com>
2022-03-06 18:21:55 +0100

    Added admin setting to turn off game-login

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-# name: basic-plugin
-# about: A test plugin for our website
+# name: game-login
+# about: Plugin to authenticate a user by providing username+password to a API endpoint. (to login to the game)
 # version: 0.0.1
 # authors: Pulse team
 # url: https://github.com/pulse-game/website-plugin
 # required_version: 2.7.0
 # transpile_js: true
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :game_login_enabled
 
 after_initialize do
 end
