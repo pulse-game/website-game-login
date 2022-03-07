0dc696eceda8ef442229979a6002aa138615dbbc
Ranama <Jeppeandreasson@gmail.com>
2022-03-07 20:23:25 +0100

    Added get_user_has_2factor_auth api endpoint

---
+++ app/controllers/game_has_2fa_controller.rb
@@ -0,0 +1,33 @@
+class GameHas2faController < ApplicationController
+	skip_before_action :preload_json, :check_xhr
+	
+	def index
+		params.require(:login)
+		params.require(:api_password)
+		
+		if params[:api_password] != SiteSetting.server_api_password  # Require password to call this API.
+			return render json:{error: "Invalid server-api-password"}
+		end
+		
+		user = User.find_by_username_or_email(normalized_login_param)
+		
+		if user.present?
+			render json: {success: true, totps: user.totps}
+		else
+			render json: {error: "Can not find user with that email/username"}
+			return
+		end
+		
+		
+	end
+	
+	def normalized_login_param
+		login = params[:login].to_s
+		if login.present?
+			login = login[1..-1] if login[0] == "@"
+			User.normalize_username(login.strip)[0..100]
+		else
+			nil
+		end
+	end
+end
--- plugin.rb
+++ plugin.rb
@@ -16,5 +16,6 @@ after_initialize do
 	Discourse::Application.routes.append do
 		# Map the path `/notebook` to `GameLoginController`’s `create` method
 		get '/game_login' => 'game_login#create'
+		get '/game_has_2fa' => 'game_has_2fa#index'
 	end
 end
